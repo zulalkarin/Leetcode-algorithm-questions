a63abf14a16e94968ad59521ea265dd4f37fef85
Zülal Karın <79747380+zulalkarin@users.noreply.github.com>
2022-08-09 16:43:05 +0300

    q26

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,15 @@
+class Solution {
+    
+  public int removeDuplicates(int[] nums) {
+        
+        int ans = 1;
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i] != nums[ans-1]) {
+                nums[ans] = nums[i];
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+}
